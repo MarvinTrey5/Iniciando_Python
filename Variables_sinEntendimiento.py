4d65c28d3db07254544978ece24c37cbb39f3372
MarvinTrey5 <santosmarvin201@gmail.com>
2024-07-29 13:55:55 -0600

    Tipos de variables no utilizables/Constante, float

---
+++ Variables_sinEntendimiento.py
@@ -0,0 +1,7 @@
+# No se puede utilizar en python.
+$numero = ""
+# No se puede utilizar en python.
+5dias = ""
+# Se tiende a utilizar en constantes.
+_Saludo = ""
+num_float = 425.22 # si fuera por entrada de teclado seria float(input("texto. "))
