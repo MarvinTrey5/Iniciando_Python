204432323de9adc10f1bf98717f6ee1ec82f4fd9
MarvinTrey5 <santosmarvin201@gmail.com>
2024-07-29 13:54:58 -0600

    Entrada de valores numéricos y una sumatoria str

---
+++ Trabajo_Numero.py
@@ -0,0 +1,8 @@
+# Sumar numeros
+print("Suma De 2 números")
+# Se debe convertir, la cadena a entero
+Primer_Numero = int(input("El primer número: "))
+Segundo_Numero = int(input("El segundo número: "))
+Tercer_Numero = int(input("El tercer número: "))
+# en concatenación hay que convertirla a cadena
+print("La  suma es: " + str(Primer_Numero + Segundo_Numero +Tercer_Numero ))
