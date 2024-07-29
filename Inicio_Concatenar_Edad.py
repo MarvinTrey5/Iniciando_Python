304dd1cc12dd48bd64d999aa65d2ff28356ee3ea
MarvinTrey5 <santosmarvin201@gmail.com>
2024-07-29 13:50:26 -0600

    Concatenación + y utilizando la clase str

---
+++ Inicio_Concatenar_Edad.py
@@ -0,0 +1,11 @@
+# Crear una variable 
+# Lo básico 
+nombre = 'Marvin'
+edad = 23
+# se concatena la variable nombre y edad
+#  pero como edad es un número se debe escribir "str" y la variable edad en paréntesis para que pueda leer bien el código.
+# y si queremos guardar todo el print en una variable pues copiamos el print pero en lugar de print le
+# asignaremos una nueva variable le llamaré 'Texto'.
+texto = 'Hola Mundo, soy ' + nombre + ' y tengo ' + str(edad) + ' Años '
+# ejecuta la función de la variable con print para obtener los datos.
+print(texto)
