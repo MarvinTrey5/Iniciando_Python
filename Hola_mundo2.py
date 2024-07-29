4ff93c6e5e30296a47214570b95417c931700581
MarvinTrey5 <santosmarvin201@gmail.com>
2024-07-29 13:49:41 -0600

    Hola mundo básico

---
+++ Hola_mundo2.py
@@ -0,0 +1,12 @@
+# Escribir un programa que pregunta 
+# el nombre de el usuario en la consola
+# y después de que el usuario introduzca
+# muestre por pantalla la cadena ¡ Hola <nombre>!,
+#  donde <nombre> es el nombre que el usuario
+# haya introducido.
+
+# Iniciamos creando la variable nombre
+#  y introduciendo el nombre (input).
+
+nombre = input("Ingrese su nombre: ")
+print("¡ Hola " + nombre + " ! ")
