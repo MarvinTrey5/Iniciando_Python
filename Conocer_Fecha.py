7342453fd88ab0cae971adfd9c609ed8c8f224ae
MarvinTrey5 <santosmarvin201@gmail.com>
2024-07-29 13:46:49 -0600

    Libreria datetime para observar la fecha correspondiente

---
+++ Conocer_Fecha.py
@@ -0,0 +1,8 @@
+# importamos el módulo date
+from datetime import date
+
+# Llamamos la función para
+# obtener la fecha de hoy.
+date.today()
+# Mostramos en consola con concatenación.
+print("Esté día tiene fecha de:  " + str(date.today()))
