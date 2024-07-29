a1a0adbedef3e9c551ab7a13a81fe3187e0ab115
MarvinTrey5 <santosmarvin201@gmail.com>
2024-07-29 13:45:47 -0600

    Visualizar 6 digítos a través de su indice

---
+++ Caracteres_Cadena.py
@@ -0,0 +1,19 @@
+# Cree un programa que al recibir datos 
+# como un número de 6 dígitos, genere
+# una impresión
+
+# Creación y variables 
+# Función input()
+# Función print()
+Can_Num = input("Ingrese los 6 dígitos: ")
+# Mostraremos los numeros en la terminal
+# de forma vertical
+
+# Tomemos en cuenta que los índices incian en 0
+# es decir 
+print(Can_Num[0])
+print(Can_Num[1])
+print(Can_Num[2])
+print(Can_Num[3])
+print(Can_Num[4])
+print(Can_Num[5])
